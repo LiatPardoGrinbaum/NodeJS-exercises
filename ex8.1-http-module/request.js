fb0a1b88e6928ba471dc921152a97736de947b4b
LiatPardoGrinbaum <pardo.liat@gmail.com>
2022-06-13 17:59:18 +0300

    added ex8.1

---
+++ ex8.1-http-module/request.js
@@ -0,0 +1,21 @@
+import https from "https";
+const url = "https://geek-jokes.sameerkumar.website/api?format=json";
+
+const request = https.request(url, (response) => {
+  let data = "";
+
+  response.on("data", (chunk) => {
+    data += chunk;
+  });
+
+  response.on("end", () => {
+    const body = JSON.parse(data);
+    console.log(body);
+  });
+});
+
+request.on("error", (error) => {
+  console.log("An error", error);
+});
+
+request.end();
